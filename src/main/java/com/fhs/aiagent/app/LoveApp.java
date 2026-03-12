fa200ecd6e808d3689582973f5077c12fd8d13eb
fhs1220888 <haosenf2@illinois.edu>
2026-03-12 01:16:58 -0500

    feat: add MCP amap tool integration and tool calling support

--- src/main/java/com/fhs/aiagent/app/LoveApp.java
+++ src/main/java/com/fhs/aiagent/app/LoveApp.java
@@ -171,4 +171,31 @@ public class LoveApp {
         log.info("content: {}", content);
         return content;
     }
+
+    // AI 调用 MCP 服务
+
+    @Resource
+    private ToolCallbackProvider toolCallbackProvider;
+
+    /**
+     * AI 报告功能（调用 MCP 服务）
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public String doChatWithMcp(String message, String chatId) {
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, chatId))
+                // 开启日志，便于观察效果
+                .advisors(new MyLoggerAdvisor())
+                .toolCallbacks(toolCallbackProvider)
+                .call()
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
 }
